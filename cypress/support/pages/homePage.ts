64e437cbc3974a4c62b26123c73141906f0470c1
yana-glt <galitsynayasya@gmail.com>
2023-09-25 14:04:33 +0200

    chore: separate each check into a separate test

--- cypress/support/pages/homePage.ts
+++ cypress/support/pages/homePage.ts
@@ -1,14 +1,14 @@
 class HomePage{
 
-    visitHomePAge(): void{
+    visitHomePAge(){
         cy.visit("/");
     }
 
-    closePopup(): void{
+    closePopup(){
         cy.get('.eom-buttons > :nth-child(1) > :nth-child(1) > yt-button-shape > .yt-spec-button-shape-next > yt-touch-feedback-shape > .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill').click();
     }
 
-    typeToSearchBar(string: string): void{
+    typeToSearchBar(string: string){
         cy.get('#search-form > #container').type(`${string}{enter}`);
     }
 }
--- cypress/support/pages/searchResultPage.ts
+++ cypress/support/pages/searchResultPage.ts
@@ -1,33 +1,32 @@
 class SearchResultPage{
 
-    isVideoEquals(firstToCompare: number, secondToCompare: number) {
-        cy.get('#contents ytd-video-renderer > #dismissible > .text-wrapper > #meta > #title-wrapper').as('listOfTitles');
+    isVideoTitlesEqual(firstToCompare: number, secondToCompare: number) {
+        cy.get('#contents > ytd-video-renderer > #dismissible > .text-wrapper > #meta > #title-wrapper').as('listOfTitles');
         cy.get('@listOfTitles').eq(firstToCompare).invoke('text').then(text => {
-            cy.get('@listOfTitles').eq(secondToCompare).invoke('text').then(text2 => {
-                expect(text).to.not.equal(text2);
-            })
-        })
+            cy.get('@listOfTitles').eq(secondToCompare).invoke('text').should('be.not.equal', text);
+        });
+    };
 
-        cy.get('#contents ytd-video-renderer > #dismissible > .text-wrapper > #channel-info > #channel-name').as('listOfChannels');
+    isVideoChannelsEqual(firstToCompare: number, secondToCompare: number) {
+        cy.get('#contents > ytd-video-renderer > #dismissible > .text-wrapper > #channel-info > #channel-name').as('listOfChannels');
         cy.get('@listOfChannels').eq(firstToCompare).invoke('text').then(text => {
-            cy.get('@listOfChannels').eq(secondToCompare).invoke('text').then(text2 => {
-                expect(text).to.not.equal(text2);
-            })
-        })
+            cy.get('@listOfChannels').eq(secondToCompare).invoke('text').should('be.not.equal', text);
+        });
+    };
 
-        cy.get('#contents ytd-video-renderer > #dismissible > .text-wrapper > .metadata-snippet-container').as('listOfDescriptions');
+    isVideoDescriptionsEqual(firstToCompare: number, secondToCompare: number) {
+        cy.get('#contents > ytd-video-renderer > #dismissible > .text-wrapper > .metadata-snippet-container').as('listOfDescriptions');
         cy.get('@listOfDescriptions').eq(firstToCompare).invoke('text').then(text => {
-            cy.get('@listOfDescriptions').eq(secondToCompare).invoke('text').then(text2 => {
-                expect(text).to.not.equal(text2);
-            })
-        })
+            cy.get('@listOfDescriptions').eq(secondToCompare).invoke('text').should('be.not.equal', text);
+        });
+    };
 
-        cy.get('#contents ytd-video-renderer > #dismissible >ytd-thumbnail #thumbnail > yt-image > img').as('listOfImages');
-        cy.get('@listOfImages').eq(firstToCompare).invoke('attr', 'src').then(text => {
-            cy.get('@listOfImages').eq(secondToCompare).invoke('attr', 'src').then(text2 => {
-                expect(text).to.not.equal(text2);
-            })
-        })
-    }
+    isVideoImagesEqual(firstToCompare: number, secondToCompare: number) {
+        cy.get('#contents > ytd-video-renderer > #dismissible > ytd-thumbnail > #thumbnail > yt-image').as('listOfImages');
+        cy.get('@listOfImages').find('img').eq(firstToCompare).invoke('attr', 'src').then(src => {
+            cy.get('@listOfImages').find('img').eq(secondToCompare).invoke('attr', 'src').should("be.not.equal", src);
+        });   
+    };
 }
+
 export default SearchResultPage;
